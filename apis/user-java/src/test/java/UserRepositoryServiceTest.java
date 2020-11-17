b8f7db3f7fb7a274e6f5f4347ba0395f63c9850b
Florian Eiden <florian.eiden@fleid.fr>
2020-11-17 15:29:21 -0800

    Pass test

--- apis/user-java/src/test/java/UserRepositoryServiceTest.java
+++ apis/user-java/src/test/java/UserRepositoryServiceTest.java
@@ -77,7 +77,7 @@ public class UserRepositoryServiceTest {
             fail("Unable to locate user");
 
         }catch (NullPointerException e) {
-            assertEquals("Unable to locate user OR NOT", e.getMessage());
+            assertEquals("Unable to locate user", e.getMessage());
         }
         verify(userRepository, never()).save(profile);
     }

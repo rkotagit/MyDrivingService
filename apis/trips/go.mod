9f7eebcbb32c009b6edba8624b6e470a2f84d6ae
JMBT <marktayl@live.com>
2020-11-19 14:17:01 -0800

    Adding app inights events

--- apis/trips/go.mod
+++ apis/trips/go.mod
@@ -1,5 +1,7 @@
 module github.com/Azure-Samples/openhack-devops-team/apis/trips
 
+go 1.15
+
 require (
 	cloud.google.com/go v0.0.0-20180627205256-45528feae6df // indirect
 	github.com/codemodus/swagui v0.1.0
@@ -7,6 +9,7 @@ require (
 	github.com/denisenkom/go-mssqldb v0.0.0-20180625034930-3724b4745ca9
 	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/mux v1.6.2
+	github.com/microsoft/ApplicationInsights-Go v0.4.3
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.1.1 // indirect
 	github.com/stretchr/testify v1.2.2
--- apis/trips/main.go
+++ apis/trips/main.go
@@ -7,12 +7,15 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/microsoft/ApplicationInsights-Go/appinsights"
+
 	sw "github.com/Azure-Samples/openhack-devops-team/apis/trips/tripsgo"
 )
 
 var (
 	webServerPort    = flag.String("webServerPort", getEnv("WEB_PORT", "8080"), "web server port")
 	webServerBaseURI = flag.String("webServerBaseURI", getEnv("WEB_SERVER_BASE_URI", "changeme"), "base portion of server uri")
+	client           = appinsights.NewTelemetryClient("618f7639-d08b-4f1f-9a64-51df20f19953")
 )
 
 func getEnv(key, fallback string) string {
@@ -25,6 +28,15 @@ func getEnv(key, fallback string) string {
 func main() {
 
 	var debug, present = os.LookupEnv("DEBUG_LOGGING")
+
+	client.Context().Tags.Cloud().SetRole("api-trips")
+
+	metriccpu := appinsights.NewMetricTelemetry("CPU Util")
+	client.Track(metriccpu)
+
+	// event := appinsights.NewEventTelemetry("button clicked")
+	// event.Properties["property"] = "value"
+	// client.Track(event)
 
 	if present && debug == "true" {
 		sw.InitLogging(os.Stdout, os.Stdout, os.Stdout)
